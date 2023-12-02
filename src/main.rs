40ecc53e0f6cb917ac7d1deb4abc7a24f6dc97eb
W4ff3l <w4ff3l@gmail.com>
2023-12-02 13:57:56 +0100

    accumulate power for second challenge

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,35 @@ const MAX_CUBES_BLUE: Cubes = Cubes {
 
 fn main() {
     let input = read_to_string("puzzle-input").unwrap();
-    let accumulated_games = accumulate_possible_games(&input);
+
+    let accumulated_games = accumulate_possible_games_c1(&input);
     println!("Accumulated possible games: {}", accumulated_games);
+
+    let power_of_minimum_cubes = accumulate_power_minimum_cubes(&input);
+    println!(
+        "Accumulated power of minimal cubes: {}",
+        power_of_minimum_cubes
+    );
 }
 
-fn accumulate_possible_games(input: &str) -> i32 {
+fn accumulate_power_minimum_cubes(input: &str) -> i32 {
+    let games = parse_games(input);
+
+    let mut accumulated_power = 0;
+
+    // Determine power of minimal needed bag configuration such that a game is possible.
+    for game in games {
+        let max_of_red_cubes = game.max_of_cubes(Color::Red);
+        let max_of_green_cubes = game.max_of_cubes(Color::Green);
+        let max_of_blue_cubes = game.max_of_cubes(Color::Blue);
+
+        accumulated_power += max_of_red_cubes * max_of_green_cubes * max_of_blue_cubes;
+    }
+
+    accumulated_power
+}
+
+fn accumulate_possible_games_c1(input: &str) -> i32 {
     let games = parse_games(input);
 
     // Determine which games are possible evaluated by the given pulls.
@@ -68,18 +92,31 @@ fn is_too_many_cubes(pull: Pull) -> bool {
 
 #[cfg(test)]
 mod tests {
-    use crate::accumulate_possible_games;
+    use crate::{accumulate_possible_games_c1, accumulate_power_minimum_cubes};
 
     #[test]
-    fn aoc_example_test() {
+    fn aoc_example_test_c1() {
         let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n\
                      Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n\
                      Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n\
                      Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n\
                      Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
 
-        let result = accumulate_possible_games(input);
+        let result = accumulate_possible_games_c1(input);
 
         assert_eq!(result, 8);
+    }
+
+    #[test]
+    fn aoc_example_test_c2() {
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n\
+                     Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n\
+                     Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n\
+                     Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n\
+                     Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+        let result = accumulate_power_minimum_cubes(input);
+
+        assert_eq!(result, 2286);
     }
 }
--- src/model.rs
+++ src/model.rs
@@ -1,4 +1,4 @@
-use std::{str::FromStr, i32};
+use std::{i32, str::FromStr};
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Color {
@@ -61,17 +61,37 @@ pub struct Game {
     pub set_of_pulls: Vec<Pull>,
 }
 
+impl Game {
+    pub fn max_of_cubes(&self, color: Color) -> i32 {
+        let mut max_cubes = 0;
+
+        for pull in &self.set_of_pulls {
+            let cubes = match color {
+                Color::Red => pull.get_num_red_cubes(),
+                Color::Green => pull.get_num_green_cubes(),
+                Color::Blue => pull.get_num_blue_cubes(),
+            };
+
+            if cubes > max_cubes {
+                max_cubes = cubes;
+            }
+        }
+
+        max_cubes
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use super::{Pull, Cubes, Color};
+    use super::{Color, Cubes, Pull, Game};
 
     #[test]
+    #[rustfmt::skip]
     fn returns_correct_color_of_cubes_from_pull() {
-        let given_red_cubes = Cubes { quantity: 1, color: Color::Red };
-        let given_green_cubes = Cubes { quantity: 2, color: Color::Green };
-        let given_blue_cubes = Cubes { quantity: 3, color: Color::Blue };
-        let set_of_cubes = vec![given_red_cubes.clone(), given_green_cubes.clone(), given_blue_cubes.clone()];
+        let given_red_cubes = Cubes { quantity: 1, color: Color::Red, };
+        let given_green_cubes = Cubes { quantity: 2, color: Color::Green, };
+        let given_blue_cubes = Cubes { quantity: 3, color: Color::Blue, };
+        let set_of_cubes = vec![ given_red_cubes.clone(), given_green_cubes.clone(), given_blue_cubes.clone(), ];
         let pull = Pull { set_of_cubes };
 
         let red_cubes = pull.get_num_red_cubes();
@@ -81,5 +101,38 @@ mod tests {
         assert_eq!(red_cubes, given_red_cubes.quantity);
         assert_eq!(green_cubes, given_green_cubes.quantity);
         assert_eq!(blue_cubes, given_blue_cubes.quantity);
+    }
+
+    #[test]
+    #[rustfmt::skip]
+    fn calculates_maximum_cubes_in_game_correctly() {
+        // given
+        let expected_red = 5;
+        let expected_green = 10;
+        let expected_blue = 20;
+
+        let given_red_cubes1 = Cubes { quantity: expected_red, color: Color::Red, };
+        let given_green_cubes1 = Cubes { quantity: 2, color: Color::Green, };
+        let given_blue_cubes1 = Cubes { quantity: 3, color: Color::Blue, };
+        let set_of_cubes1 = vec![ given_red_cubes1.clone(), given_green_cubes1.clone(), given_blue_cubes1.clone(), ];
+        let pull1 = Pull { set_of_cubes: set_of_cubes1 };
+
+        let given_red_cubes2 = Cubes { quantity: 1, color: Color::Red, };
+        let given_green_cubes2 = Cubes { quantity: expected_green, color: Color::Green, };
+        let given_blue_cubes2 = Cubes { quantity: expected_blue, color: Color::Blue, };
+        let set_of_cubes2 = vec![ given_red_cubes2.clone(), given_green_cubes2.clone(), given_blue_cubes2.clone(), ];
+        let pull2 = Pull { set_of_cubes: set_of_cubes2 };
+
+        let game = Game { index: 1, set_of_pulls: vec![pull1, pull2]  };
+        
+        // when
+        let result_red = game.max_of_cubes(Color::Red);
+        let result_green = game.max_of_cubes(Color::Green);
+        let result_blue = game.max_of_cubes(Color::Blue);
+
+        // then
+        assert_eq!(result_red, expected_red);
+        assert_eq!(result_green, expected_green);
+        assert_eq!(result_blue, expected_blue);
     }
 }
