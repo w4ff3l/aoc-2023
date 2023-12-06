5fa9ff11ea992a1c163790ddb659064ad17fbbcf
W4ff3l <w4ff3l@gmail.com>
2023-12-06 21:52:02 +0100

    day 6 challenge 2 done

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,15 +1,39 @@
-use std::{fs::read_to_string, i32};
+use std::{fs::read_to_string, u64};
 
 fn main() {
     let input = read_to_string("puzzle-input").unwrap();
 
     let result_challenge_1 = calculate_result_challenge_1(&input);
     println!("Result for challenge 1: {}", result_challenge_1);
+    let result_challenge_2 = calculate_result_challenge_2(&input);
+    println!("Result for challenge 2: {}", result_challenge_2);
 }
 
-fn calculate_result_challenge_1(input: &str) -> i32 {
+fn calculate_result_challenge_2(input: &str) -> u64 {
+    let race = parse_race(input);
+
+    println!("Race start");
+    let max_time = race.time;
+    let max_distance = race.distance;
+
+    let mut c_ways_to_win = 0;
+
+    for hold in 0..max_time {
+        let distance = hold * (max_time - hold);
+        if distance > max_distance {
+            if hold % 10000 == 0 {
+                println!("Progress: {:?}/{:?}", hold, max_time);
+            }
+            c_ways_to_win += 1;
+        }
+    }
+
+    c_ways_to_win
+}
+
+fn calculate_result_challenge_1(input: &str) -> u64 {
     let races = parse_races(input);
-    let mut ways_to_win_per_race: Vec<i32> = Vec::new();
+    let mut ways_to_win_per_race: Vec<u64> = Vec::new();
 
     for race in races {
         println!("New Race");
@@ -21,7 +45,7 @@ fn calculate_result_challenge_1(input: &str) -> i32 {
         for hold in 0..max_time {
             let distance = hold * (max_time - hold);
             if distance > max_distance {
-            println!("Hold/Distance: {:?}/{:?}", hold, distance);
+                println!("Hold/Distance: {:?}/{:?}", hold, distance);
                 c_ways_to_win += 1;
             }
         }
@@ -30,14 +54,32 @@ fn calculate_result_challenge_1(input: &str) -> i32 {
         println!();
         ways_to_win_per_race.push(c_ways_to_win);
     }
-    
+
     ways_to_win_per_race.iter().product()
 }
 
 struct Race {
-    time: i32,
-    distance: i32,
+    time: u64,
+    distance: u64,
 }
+
+fn parse_race(input: &str) -> Race {
+    let split_time_distance = input.split("\n").collect::<Vec<&str>>();
+
+    let time = split_time_distance[0].split("Time:").collect::<Vec<&str>>()[1]
+        .replace(" ", "")
+        .parse::<u64>()
+        .unwrap();
+    let distance = split_time_distance[1]
+        .split("Distance:")
+        .collect::<Vec<&str>>()[1]
+        .replace(" ", "")
+        .parse::<u64>()
+        .unwrap();
+
+    Race { time, distance }
+}
+
 fn parse_races(input: &str) -> Vec<Race> {
     let split_time_distance = input.split("\n").collect::<Vec<&str>>();
 
@@ -45,16 +87,16 @@ fn parse_races(input: &str) -> Vec<Race> {
         .split_ascii_whitespace()
         .collect::<Vec<&str>>()
         .iter()
-        .map(|str| str.parse::<i32>().unwrap())
-        .collect::<Vec<i32>>();
+        .map(|str| str.parse::<u64>().unwrap())
+        .collect::<Vec<u64>>();
     let distances = split_time_distance[1]
         .split("Distance:")
         .collect::<Vec<&str>>()[1]
         .split_ascii_whitespace()
         .collect::<Vec<&str>>()
         .iter()
-        .map(|str| str.parse::<i32>().unwrap())
-        .collect::<Vec<i32>>();
+        .map(|str| str.parse::<u64>().unwrap())
+        .collect::<Vec<u64>>();
 
     let mut races: Vec<Race> = Vec::new();
     for (time, distance) in times.iter().zip(distances) {
@@ -69,7 +111,7 @@ fn parse_races(input: &str) -> Vec<Race> {
 
 #[cfg(test)]
 mod tests_c1 {
-    use crate::{parse_races, calculate_result_challenge_1};
+    use crate::{calculate_result_challenge_1, parse_races};
 
     #[test]
     fn challenge_1_example_test() {
